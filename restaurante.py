2980c3adaf64b07ecd6dd7e71303cf909912ee2e
kventura <kissilasventura@gmail.com>
2014-02-14 18:11:59 -0200

    commit class: conta, itemconta, produto

--- restaurante.py
+++ restaurante.py
@@ -21,3 +21,30 @@ class Garcom:
 		self.identidade = identidade
 		self.matricula = matricula
 		self.contas = contas
+		
+class Conta:
+	def __init__(self,data_abertura,hora_abertura,status,mesa,garcom,hora_fechamento=None,data_fechamento=None,valor_final=0.0,comissao=0.1,itens=[]):
+		self.data_abertura = data_abertura
+		self.hora_abertura = hora_abertura
+		self.status = status
+		self.mesa = mesa
+		self.garcom = garcom
+		self.hora_fechamento = hora_fechamento
+		self.data_fechamento = data_fechamento
+		self.itens = itens
+		self.valor_final = valor_final
+		self.comissao = comissao
+		
+class Produto:
+	def __init__(self,descricao,codigo,preco,itens=[],categorias=[]):
+		self.descricao = descricao
+		self.codigo = codigo
+		self.preco = preco
+		self.itens = itens
+		self.categorias = categorias
+		
+class ItemConta:
+	def __init__(self,quantidade,produto,conta):
+		self.quantidade = quantidade
+		self.produto = produto
+		self.conta = conta
--- spec/restaurante_spec.py
+++ spec/restaurante_spec.py
@@ -31,3 +31,42 @@ class GarcomSpec(unittest.TestCase):
 		garcom.identidade |should| equal_to('identidade')
 		garcom.matricula |should| equal_to('matricula')
 		garcom.contas |should| equal_to([])
+		
+class ContaSpec(unittest.TestCase):
+
+	def it_creates_a_conta_object(self):
+		mesa = Mesa('numero','status',[])
+		garcom = Garcom('nome','endereco','telefone','cpf','identidade','matricula',[])
+		conta = Conta('dd/mm/aaaa','hh:mm:ss','status',mesa,garcom)
+		conta.data_abertura |should| equal_to('dd/mm/aaaa')
+		conta.hora_abertura |should| equal_to('hh:mm:ss')
+		conta.status |should| equal_to('status')
+		conta.mesa |should| equal_to(mesa)
+		conta.garcom |should| equal_to(garcom)
+		conta.hora_fechamento |should| equal_to(None)
+		conta.data_fechamento |should| equal_to(None)
+		conta.valor_final |should| equal_to(0.0)
+		conta.comissao |should| equal_to(0.1)
+		conta.itens |should| equal_to([])
+
+class ProdutoSpec(unittest.TestCase):
+
+	def it_creates_a_produto_object(self):
+		produto = Produto('descricao','codigo','preco')
+		produto.descricao |should| equal_to('descricao')
+		produto.codigo |should| equal_to('codigo')
+		produto.preco |should| equal_to('preco')
+		produto.itens |should| equal_to([])
+		produto.categorias |should| equal_to([])
+
+class ItemContaSpec(unittest.TestCase):
+
+	def it_creates_a_item_conta_object(self):
+		produto = Produto('descricao','codigo','preco',[],[])
+		mesa = Mesa('numero','status',[])
+		garcom = Garcom('nome','endereco','telefone','cpf','identidade','matricula',[])
+		conta = Conta('dd/mm/aaaa','hh:mm:ss','status',mesa,garcom,None,None,0.0,0.1,[])
+		itemconta = ItemConta('quantidade',produto,conta)
+		itemconta.quantidade |should| equal_to('quantidade')
+		itemconta.produto |should| equal_to(produto)
+		itemconta.conta |should| equal_to(conta)
